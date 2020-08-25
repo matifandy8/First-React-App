64a893ccf1c1426403b9907ccd02cccd4600e80c
matifandy <matifandy@gmail.com>
2020-08-25 00:03:27 -0300

    todo and postapi

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,8 +4,14 @@ import Task from './Task';
 
 class Tasks extends Component {
     render() {
-        return this.props.tasks.map(task => <Task task={task} key={task.id}/>);
+        return this.props.tasks.map(task => 
+        <Task 
+        task={task} 
+        key={task.id} 
+        deleteTask={this.props.deleteTask} checkDone={this.props.checkDone}/>);
     }
 }
+
+
 
 export default Tasks;
